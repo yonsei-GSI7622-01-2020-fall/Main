e6323881a0535e70394374016212d6d3c7c1f5d3
서중원 <wsbtgt@gmail.com>
2020-10-17 17:26:27 +0900

    Update install.sh

--- assignment/week-05/image_ocr/install.sh
+++ assignment/week-05/image_ocr/install.sh
@@ -7,11 +7,14 @@ sudo apt-get install libopenblas-dev liblapack-dev -y
 sudo apt-get install libx11-dev libgtk-3-dev -y
 sudo apt-get install python python-dev python-pip -y
 sudo apt-get install python3 python3-dev python3-pip -y
+sudo apt-get install python3-venv -y
 python3 -m venv venv
 . venv/bin/activate
+pip install pip --upgrade 
 pip install Flask
 pip install Pillow
 pip install scikit-build
 pip install opencv-contrib-python
 pip install numpy
+pip install torch --no
 pip install easyocr
